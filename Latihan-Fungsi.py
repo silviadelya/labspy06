92f95fd6f287c543eb71c59e17d265db259640ac
silviadelya <silviadelyaheryani31@gmail.com>
2021-12-11 22:56:44 +0700

    program

---
+++ Latihan-Fungsi.py
@@ -0,0 +1,23 @@
+print("Latihan - Fungsi")
+print()
+
+# def a(x):
+#    return x**2
+a = lambda x : x**2
+print(a(6))
+
+# def b(x, y):
+#    return math.sqrt(x**2 + y**2)
+b = lambda x, y : x**2 + y**2
+print(b(7, 2))
+
+# def c(*args):
+#   return sum(args)/len(args)
+c = lambda *args : sum(args)/len(args)
+print(c(5, 10, 25))
+
+# def d(s):
+#    return "".join(set(s))
+d = lambda s : "".join(set(s))
+print(d("Apel"))
+        
